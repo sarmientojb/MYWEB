fd376f9b1beec9abb7e091bc2f23ee7010cb1056
sarmientojb <sarmiento.jb@gmail.com>
2025-05-05 14:59:10 +0800

    Add files via upload
    Signed-off-by: sarmientojb <sarmiento.jb@gmail.com>

---
+++ Test.js
@@ -0,0 +1,21 @@
+
+/*/unction myFunction() {
+  var x = document.getElementById("MyDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction() {
+  var x = document.getElementById("MyDiv2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}*/
+
+
